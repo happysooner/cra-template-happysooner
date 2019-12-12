296ba065a0a3d41ecfe4838252c5b37db7b488b4
blessyou <370732889@qq.com>
2019-12-12 11:39:56 +0800

    improvement: 精简type

--- src/page/about/index.tsx
+++ src/page/about/index.tsx
@@ -2,10 +2,7 @@ import { RouteComponentProps } from "@reach/router";
 import React from "react";
 import DefaultLayout from "components/Layout/default";
 
-interface AboutIProps {
-  uid?: string;
-}
-const About = (props: RouteComponentProps & AboutIProps) => {
+const About = (props: RouteComponentProps<{ uid: string }>) => {
   return (
     <DefaultLayout>
       <div>无情的关于页面</div>
